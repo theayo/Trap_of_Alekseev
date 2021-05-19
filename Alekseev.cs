2d98dab514090e0ab61a463063ececdc44b0a739
theayo <73176420+theayo@users.noreply.github.com>
2021-05-19 10:15:26 +0300

    Update Alekseev.cs

--- Alekseev.cs
+++ Alekseev.cs
@@ -1,12 +1,64 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
-namespace NoCap_3_5_6_8_
+namespace NoCap_nte_framework_
 {
     class Alekseev
     {
-  public void initOpenBrov()
+        public string OpenBrov = "";
+        public string Open = "";
+        public string Close = "";
+        public void quest(string str, Alekseev alex)
+        {
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.White;
+
+            string output = "";
+            this.initOpen();
+            this.initClose();
+            this.initOpenBrov();
+            int i = 0;
+            while (output.Length != str.Length)
+            {
+                System.Threading.Thread.Sleep(70);
+                this.paintAlekseev();
+                Console.SetCursorPosition(0, 0);
+                if (i % 6 == 0) //моргнул
+                {
+                    this.paintAleksZakroiBrov();
+                    Console.SetCursorPosition(0, 0);
+                    System.Threading.Thread.Sleep(70);
+                }
+                if (i % 8 == 0) //моргнул
+                {
+                    this.paintAleksZakroiBrov();
+                    Console.SetCursorPosition(0, 0);
+                    System.Threading.Thread.Sleep(90);
+                }
+                if (i % 4 == 0 || i % 7 == 0)
+                    System.Threading.Thread.Sleep(200);
+                this.paintAlekseev();
+                Console.SetCursorPosition(5, 45);
+                output += str[i];
+                Console.WriteLine("\n");
+                Console.WriteLine(output);
+                Console.SetCursorPosition(0, 0);
+                ++i;
+            }
+            Menu menu = new Menu();
+            while (true)
+            {
+                alex.paintAlekseev();
+                Console.WriteLine();
+                Console.SetCursorPosition(0, 47);
+                Console.WriteLine(str + " \n"); //pashuk
+                string str0 = menu.drawMenu("suka", "suka", "suka", 49);
+            }
+        }
+        public void initOpenBrov()
         {
             OpenBrov += "@@@@@#@@@@@@@@@@#@@@@@#%===@#=++++++++@###==++++++*=%+**%%=========%%%%%===%%++++++";
             OpenBrov += "#@@#####@@@@@######@#@@@@@#@%%*++++++@###==++++++++%++*%%%@%======%%========+*++++*";
@@ -212,7 +264,7 @@ namespace NoCap_3_5_6_8_
 
         public void paintAlekseev()
         {
-          //  Console.Clear();
+            //  Console.Clear();
             int i = 0;
             while (Close.Length != i)
             {
@@ -222,7 +274,7 @@ namespace NoCap_3_5_6_8_
                 i++;
             }
         }
-        
+
         public void paintAleksZakroiRot()
         {
             int i = 0;
